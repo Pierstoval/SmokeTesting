b1043edb1c62a82171ca55f3e413d0631ec30610
Alex Rock Ancelet <pierstoval@gmail.com>
2025-02-14 11:45:20 +0100

    Add php 8.3 and 8.4 to the CI

--- src/SmokeTestingBundle.php
+++ src/SmokeTestingBundle.php
@@ -2,7 +2,6 @@
 
 namespace Pierstoval\SmokeTesting;
 
-use Pierstoval\SmokeTesting\Command\MakeSmokeTestCommand;
 use Pierstoval\SmokeTesting\Maker\MakeSmokeTests;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -12,11 +11,6 @@ class SmokeTestingBundle extends Bundle
     public function build(ContainerBuilder $container): void
     {
         $container->register(MakeSmokeTests::class)
-            ->setAutowired(true)
-            ->setAutoconfigured(true)
-        ;
-        $container->register(MakeSmokeTestCommand::class)
-            ->setPublic(true)
             ->setAutowired(true)
             ->setAutoconfigured(true)
         ;
